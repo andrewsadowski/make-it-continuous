c90c454afd266aa85a70b267c87d7ae8a3c2f209
admin <andrew.n.sadowski@gmail.com>
2018-11-27 12:39:23 -0800

    added header div, updated main to use loadURL instead of loadFile

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({ width: 430, height: 445, resizable: false });
 
   // and load the index.html of the app.
-  mainWindow.loadFile("index.html");
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
